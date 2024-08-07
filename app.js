cbf33bd6d5390bd725299b4854168e94373b572c
Michael <michael.alexander0008@gmail.com>
2024-08-07 11:52:50 +0100

    Complete Exercise 11.12 with failing deployment

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('1')
+  res.send('12')
+})
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw new Error('Oh no!')
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
