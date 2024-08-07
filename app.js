178995c95606ba33b46fae41d38b0d953e69df6f
Michael <michael.alexander0008@gmail.com>
2024-08-07 14:28:13 +0100

    Commit made for purpose of learning PRs

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('12')
+  res.send('I dunno dude. Sorry.')
 })
 
 app.get('/health', (req, res) => {
